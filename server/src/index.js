ed9ded3e1046dfb4c7ddef0603ede038d448be21
Leticia Soares <leticia.costa@fatec.sp.gov.br>
2020-09-12 08:51:39 -0300

    Update index.js

--- server/src/index.js
+++ server/src/index.js
@@ -4,13 +4,6 @@ var book = require('./routes/book');
 
 const app = express();
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "*");
-    res.header("Access-Control-Allow-Methods", "*");
-    next();
-});
-
 app.use(bodyParser.json())
 
 app.get("/ping", (request, response) => {
