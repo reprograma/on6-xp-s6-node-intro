ec214c2c770d035f4c6e99144f4e0c3cd53baed9
luzialeticia <leticialuzia@tutamail.com>
2020-09-18 04:24:46 -0300

    Corringodo nomes de algumas variáveis

--- exercicios-obrigatorio/index.js
+++ exercicios-obrigatorio/index.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const users = require('./routes/users')
 const app = express()
-const PORT = 3000
+const PORT = 3001
 
 app.use(bodyParser.json())
 app.listen(PORT, () => console.log(`App listening on port ${PORT}!`))
 
-app.post('/users', users.login)
+app.post('/user', users.login)
--- exercicios-obrigatorio/routes/users.js
+++ exercicios-obrigatorio/routes/users.js
@@ -1,11 +1,16 @@
-//const { request } = require("express");
+const { request, response } = require("express");
 
-const users = []
+const users = [
+    {
+        email: 'lelus@lulu.com',
+        password: 12345
+    }
+]
 
 const login = (request, response) => {
     const user = request.body
     let findUser = false
-    let findPassword
+    let findPassword = false
 
     users.find(item => {
         if (item.email === user.email && item.password === user.password) {
