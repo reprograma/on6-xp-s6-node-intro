9c6ef0fa0944654339cf9dad26fa7ced85aacb39
luzialeticia <leticialuzia@tutamail.com>
2020-09-18 03:12:04 -0300

    Declarando a variável do body-parser e chamando com app.use

--- exercicios-obrigatorio/index.js
+++ exercicios-obrigatorio/index.js
@@ -1,5 +1,7 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
 const PORT = 3000
 
+app.use(bodyParser.json())
 app.listen(PORT, () => console.log(`App listening on port ${PORT}!`))
