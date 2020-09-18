7349a11a1ba84012baa2b26496769dc031a5d987
Jessyca Maria Pascoa <50502920+pascoa21@users.noreply.github.com>
2020-09-18 16:57:10 -0300

    Exibir lista de tarefas e criar nova tarefa

--- entrega/exercicioDaSemana.js
+++ entrega/exercicioDaSemana.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const { response, request } = require('express')
 
 const app = express()
+
 app.use(bodyParser.json())
 
 const PORT = 5001
@@ -10,7 +12,7 @@ app.listen(PORT,function () {
     console.log('Servidor disponivel.')
 })
 
-const usuario =[
+const usuariCadastrado =[
     {
         name: 'Jessyca Pascoa',
         email: 'jessyca_pascoa@hotmail.com',
@@ -34,11 +36,25 @@ const usuario =[
     }
 ]
 
+const listaTarefa =[
+    {
+        title: 'Atividade para fazer até sexta',
+        description: 'Fazer as atividades da reprograma',
+        Local: 'em casa'
+
+    },
+    {
+        title: 'Médico',
+        description: 'Consulto com o médice',
+        Local: 'Boa Viagem'
+    }
+]
+
 const FazerLogin = (request,response)=> {
     const usuario = request.body;
     let validoUsuario = false;
     let senhaCorreta = false;
-    usuario.find(element => {
+    usuariCadastrado.find(element => {
         if(element.email === usuario.email && element.senha === usuario.senha){
             validoUsuario = true;
             senhaCorreta = true
@@ -55,8 +71,23 @@ const FazerLogin = (request,response)=> {
         }
     })
 }
+const novaTarefa = (request,response)=>{
+    const tarefa = request.body
+    listaTarefa.push(tarefa)
+    if (listaTarefa.title) {
+        return response.status(201).send({message : 'Atividade cadastrada com Sucesso!'})        
+    } else {
+        return response.status(400).send({message : 'Faltou titulo da tarefa.' })
+    }
+}
+const visualizarLista = (request,response) =>{
+    return response.status(200).send(listaTarefa)
+}
 
-app.post('/login', FazerLogin)
+app.post('/enta/login', FazerLogin)
+app.get('/enta/todas', visualizarLista);
+app.post('/entrega/nova', novaTarefa);
+
 
 
 
