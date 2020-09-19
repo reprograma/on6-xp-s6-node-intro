ed8c818a61faa04bf478f48ce8ea1adb8c1604d0
luzialeticia <leticialuzia@tutamail.com>
2020-09-18 23:00:39 -0300

    Criando a lógica para validação do usuário

--- exercicios-obrigatorio/routes/signUp.js
+++ exercicios-obrigatorio/routes/signUp.js
@@ -5,4 +5,18 @@ const users = []
 const signUp = (request, response) => {
     const user = request.body
     users.push(user)
+
+    if(!user.name) {
+        return response.status(400).send ({ message : 'Digite um nome válido'})
+    } else if(!user.email) {
+        return response.status(400).send ({ message : 'Digite um e-mail válido'})
+    } else if (!user.password) {
+        return response.status(400).send ({ message : 'Digite uma senha válida'})
+    } else {
+        return response.status(201).send({ message :  'Usuário Cadastrado com sucesso!'})
+    }
+}
+
+module.exports = {
+    signUp
 }
