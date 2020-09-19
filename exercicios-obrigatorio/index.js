bc7fa72f72040cf655882383db556ab6d163fbe4
luzialeticia <leticialuzia@tutamail.com>
2020-09-18 23:04:41 -0300

    Editando rota para sign up

--- exercicios-obrigatorio/index.js
+++ exercicios-obrigatorio/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const login = require('./routes/login')
-const signUp = require('./routes/signUp')
+const signUp = require('./routes/signup')
 const app = express()
 const PORT = 3001
 
