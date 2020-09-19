914d46a4145f21d34720f1c089c443a6c1053b0a
luzialeticia <leticialuzia@tutamail.com>
2020-09-18 23:26:31 -0300

    Criando a l´ógica para validação da todo list

--- exercicios-obrigatorio/routes/todolist.js
+++ exercicios-obrigatorio/routes/todolist.js
@@ -4,18 +4,37 @@ const { request, response } = require("express");
 const todos = [
     {
         title: "Estudar",
-        text: "Fazer desafio de APIs"
+        text: "- Fazer desafio de APIs\n- Fazer o pull request"
     },
     {
         title: "Enviar currículo",
-        text: "Preparar currículo e colocar no linkedin"
+        text: "- Preparar currículo\n- Colocar no linkedin"
     }
 ]
 
+//Lista Todos List
 const listTodos = (request, response) => {
     return response.status(200).send(todos)
 }
 
+
+//Cria nova Todo List
+const newTodo = (request, response) => {
+    const todo = request.body
+    todos.push(todo)
+
+    if(!todo.title) {
+        return response.status(400).send ({ message : 'Digite um título para sua Todo List'})
+    } else if(!todo.text) {
+        return response.status(400).send ({ message : 'Inclua uma tarefa em sua Todo List'})
+    } else {
+        return response.status(201).send({ message :  'Todo List criada!'})
+    }
+}
+
+
+
 module.exports = {
-    listTodos
+    listTodos,
+    newTodo
 }
