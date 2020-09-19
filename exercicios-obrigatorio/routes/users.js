645922cd1164c2ada4f26d70e30ac0f70b2f5a32
luzialeticia <leticialuzia@tutamail.com>
2020-09-18 23:57:07 -0300

    Juntando os arquivos de login e cadatros em um só (users)

--- exercicios-obrigatorio/routes/users.js
+++ exercicios-obrigatorio/routes/users.js
@@ -1,5 +1,7 @@
 const { request, response } = require("express");
 
+
+//Lista de usuários
 const users = [
     {
         email: 'lelus@lulu.com',
@@ -7,6 +9,8 @@ const users = [
     }
 ]
 
+
+//Fazer login
 const login = (request, response) => {
     const user = request.body
     let findUser = false
@@ -26,6 +30,26 @@ const login = (request, response) => {
     }
 }
 
+
+
+//Cria cadastro
+const signUp = (request, response) => {
+    const user = request.body
+    users.push(user)
+
+    if(!user.name) {
+        return response.status(400).send ({ message : 'Digite um nome válido'})
+    } else if(!user.email) {
+        return response.status(400).send ({ message : 'Digite um e-mail válido'})
+    } else if (!user.password) {
+        return response.status(400).send ({ message : 'Digite uma senha válida'})
+    } else {
+        return response.status(201).send({ message :  'Usuário Cadastrado com sucesso!'})
+    }
+}
+
+
 module.exports = {
-    login
+    login,
+    signUp
 }
