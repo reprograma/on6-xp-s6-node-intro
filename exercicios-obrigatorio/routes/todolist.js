abf8d0f81d666eebb9637ca530c3485f4c614f77
luzialeticia <leticialuzia@tutamail.com>
2020-09-18 23:10:08 -0300

    Criando função para listar e um array com todos para serem listados

---
+++ exercicios-obrigatorio/routes/todolist.js
@@ -0,0 +1,17 @@
+const { request, response } = require("express");
+
+
+const todos = [
+    {
+        title: "Estudar",
+        text: "Fazer desafio de APIs"
+    },
+    {
+        title: "Enviar currículo",
+        text: "Preparar currículo e colocar no linkedin"
+    }
+]
+
+const listTodos = (request, response) => {
+    return response.status(200).send(todos)
+}
